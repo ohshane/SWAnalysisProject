168ccbaa7715771cfb913c5df3910473abc37447
tim0114gap <tim0114gap@gmail.com>
2018-11-26 17:16:58 +0900

    create new methods

--- LibraryManagementSystem/src/Borrower.java
+++ LibraryManagementSystem/src/Borrower.java
@@ -1,3 +1,33 @@
 public class Borrower {
     private String name;
+    
+    public Borrower(String name)
+    {
+    	
+    }
+    
+    public void attachBook()
+    {
+    	
+    }
+    
+    public void detachBook()
+    {
+    	
+    }
+    
+    public int compareTo(Object obj)
+    {
+    	return (int) obj;//instead value
+    }
+    
+    public boolean equals(Object obj)
+    {
+    	return (boolean) obj;//instead value
+    }
+    
+    public int hashCode()
+    {
+    	return 1;//instead value
+    }
 }
