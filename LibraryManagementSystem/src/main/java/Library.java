2bf32707fe9eb5c81a6df30eea3db7980c2bafb4
tim0114gap <tim0114gap@gmail.com>
2018-11-26 16:04:07 +0900

    add new methods

--- LibraryManagementSystem/src/main/java/Library.java
+++ LibraryManagementSystem/src/main/java/Library.java
@@ -1,3 +1,38 @@
 public class Library {
     private String name;
+    
+    public Library(String name)
+    {
+    	
+    }
+    
+    public void registerOneBorrwer(String borrowerName)
+    {
+    	
+    }
+    
+    public void addOneBook(String title, String author, int uniqueCatalogueNumber)
+    {
+    	
+    }
+    
+    public void lendOneBook(String borrwerName, Book book)
+    {
+    	
+    }
+    
+    public void returnOneBook(Book book)
+    {
+    	
+    }
+    
+    public void displayBooksAvailableForLoan()
+    {
+    	
+    }
+    
+    public void displayBooksForLoan()
+    {
+    	
+    }
 }
