0d5b1e1c9f6eef6002b4f6f6f5ba3c762b1c3564
ukidaruma97 <ukidaruma97@naver.com>
2018-11-26 17:19:25 +0900

    modified:   Book.java

--- LibraryManagementSystem/src/Book.java
+++ LibraryManagementSystem/src/Book.java
@@ -3,8 +3,15 @@ public class Book {
 	private String[] author;
 	private String title;
 
-	public Book() {
+	public Book(String title, String[] author, int catalogueNumber) {
 
 	}
+	
+	public void display() {}
+	public void attatchBorrower() {}
+	public void detatchBorrower() {}
+	public void getBorrower() {}
+	public int compareTo(Object obj) {}
+	public boolean equals(Object obj) {}
 }
  
